b5a272361fa7706ea7d029eb7e0f5c560d231762
AKToran <aktoran60@gmail.com>
2025-04-08 11:41:20 +0600

    event handling in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,39 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
-  
+  function handleClick() {
+    alert("button the first");
+  }
+
+  const handleClickProper = () => {
+    alert("we will use this one.");
+  };
+
+  const addFive = (num) =>{
+    let newNum = num + 5;
+    alert(newNum);
+  }
 
   return (
     <>
+      <h3>Vite + React</h3>
+      <button onClick={handleClick}>Click Me</button>
+      <button
+        onClick={() => {
+          alert("button the second");
+        }}
+      >
+        Click Me
+      </button>
+
+      <button onClick={handleClickProper}>Proper</button>
+
+      <button onClick={addFive(5)}>Add</button>
       
-      <h1>Vite + React</h1>
-      
+      {/* proper way to pass an argument */}
+      <button onClick={()=> addFive(5)}>Add</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
